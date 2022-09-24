cc0aaaa27dd8d3df7095a9a6fd00f1510c19e675
aleena <aleenajohns31@gmail.com>
2022-09-24 16:25:37 -0700

    finished category model

--- models/index.js
+++ models/index.js
@@ -5,17 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsToMany(Category, {
-  through: {
-    model: Category, 
-    unique: false
-  }
-})
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
