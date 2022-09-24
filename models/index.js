dbb229e21a465bd683a5d1e35fdb647ac1066f6a
aleena <aleenajohns31@gmail.com>
2022-09-24 08:54:08 -0700

    starter code

---
+++ models/index.js
@@ -0,0 +1,25 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsToMany(Category, {
+  through: {
+    model: Category, 
+    unique: false
+  }
+})
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
